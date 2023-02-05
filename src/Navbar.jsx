f4fdac0a5069f5fc58c455e8c9272eaab653a81c
Saud Alshehri <70582252+SaudFi@users.noreply.github.com>
2023-02-05 15:36:36 +0300

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,89 @@
+import { Disclosure, } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'About', href: '#', current: false },
+    { name: 'FAQ', href: '#', current: false },
+    { name: 'Contact Us', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+    return (
+        <Disclosure as="nav" className="bg-background border-b-2 border-dark-green font-Roboto">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 ">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6 text-jetBlack" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6 text-jetBlack" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex items-center md:mr-auto">
+                                    <p className='font-NotoSerif text-2xl'>BetterKFUPM</p>
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4 ">
+                                        {navigation.map((item) => (
+                                            <a
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? ' text-mid-green' : 'text-jetBlack hover:text-mid-green',
+                                                    'px-3 py-2 text-md'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="hidden md:block absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                <button
+                                    type="button"
+                                    className=" rounded-lg bg-mid-green py-2 px-5 text-white font-Montserrat hover:bg-dark-green active:translate-y-1"
+                                >
+                                    Get Started
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pt-2 pb-3">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'text-mid-green' : 'text-jetBlack hover:text-mid-green',
+                                        'block px-3 py-2'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+    )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#dad7cd",
+        "dark-green": "#344e41",
+        "light-green": "#a3b18a",
+        "mid-green": "#588157",
+        jetBlack: "#2a2b2e",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        NotoSerif: ["Noto Serif", "serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
