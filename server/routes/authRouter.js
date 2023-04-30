bcf8c83e7b5deb0ccf12ce1bef34bc8f8782a24c
Ali Alnujaidi <Ali.Alnujaidi4@gmail.com>
2023-04-30 08:18:15 +0300

    ..

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {signupUser, loginUser} from '../Controllers/authController.js'
+import {signupUser, loginUser, resetPassword, updatePassword} from '../Controllers/authController.js'
 const router = express.Router()
 
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
+router.post('/reset-password', resetPassword);
+router.post('/update-password', updatePassword);
 
 export default router
